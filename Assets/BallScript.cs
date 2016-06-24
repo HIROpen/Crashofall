bba0b8823db5cbcadb3413e3aba2c5c7506662ac
HIROpen <hiropen1996@gmail.com>
2016-06-24 21:47:09 +0900

    firstcommit

---
+++ Assets/BallScript.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallScript : MonoBehaviour {
+
+        // Use this for initialization
+        void Start () {
+                GetComponent<Rigidbody>().velocity = new Vector3(0, 10, 15);
+        }
+
+        // Update is called once per frame
+        void Update () {
+
+        }
+
+}
---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+        public GameObject ball;
+        public Camera mycamera;
+        public float timer;
+        // Use this for initialization
+        void Start () {
+                //Destroy(gameObject,5);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+
+                if (Input.GetMouseButton(0))
+                {
+                        timer += Time.deltaTime;
+                        if(timer>0.1) {
+                                Vector3 v3 = Input.mousePosition;
+                                v3.z = 5;
+                                v3 = mycamera.ScreenToWorldPoint(v3);
+                                Debug.Log(v3);
+
+                                Instantiate(ball, v3, transform.rotation);
+
+                                timer = 0;
+                        }
+                }
+        }
+}
